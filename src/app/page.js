a9bcdb86371990916f056338e2e0f049520732cd
Jiraphat Ponrat <jiraphat.ponrat@gmail.com>
2023-09-02 14:24:40 +0700

    .

--- src/app/page.js
+++ src/app/page.js
@@ -21,6 +21,7 @@ import { useForm, zodResolver } from "@mantine/form";
 import { useDisclosure } from "@mantine/hooks";
 import { z } from "zod";
 import { runningPlans } from "../libs/runningPlans";
+import path from "path";
 
 const schema = z
   .object({
@@ -44,7 +45,10 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z
+      .string()
+      .min(3, { message: "Password must contain atleast 6 characters" })
+      .max(12, { message: "Password must not exceed 12 characters" }),
     confirmPassword: z.string(),
   })
   .refine(
@@ -62,6 +66,16 @@ const schema = z
     {
       message: "Invalid coupon code",
       path: ["coupon"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.confirmPassword !== data.password) return false;
+      if (data.confirmPassword === data.password) return true;
+    },
+    {
+      message: "Password does not match",
+      path: ["confirmPassword"],
     }
   );
 
@@ -90,10 +104,15 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
+
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
 
     //check discount here
+    if (form.values.hasCoupon && form.values.coupon === "CMU2023") price *= 0.7;
 
     return price;
   };
